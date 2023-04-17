4c58a9a3f3d0b35f131e4a31fef3891f20f7b155
liuyihui <liuyihui02@gmail.com>
2023-04-17 13:11:25 +0800

    remove: webserver

--- step-7-webserver/ek-tm4c1294xl/README.md
+++
@@ -1,4 +0,0 @@
-# EK-TM4C1294XL baremetal firmware
-
-For flashing, download and install [Uniflash](https://mongoose.ws/tutorials/ti/ek-tm4c1294xl-baremetal/#build-and-run) utility.
-
--- step-7-webserver/nucleo-f429zi/mongoose_custom.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 Cesanta Software Limited
-// All rights reserved
-
-#pragma once
-#define MG_ARCH MG_ARCH_NEWLIB
-#define MG_ENABLE_MIP 1
-#define MG_ENABLE_PACKED_FS 1
-#define MG_IO_SIZE 512
-#define MG_ENABLE_CUSTOM_MILLIS 1
